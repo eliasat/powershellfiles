f88cef4e7c798f066b97a77e29e06db75a70672f
Elias Al-Tai <elias.altai@dekiru.se>
2020-07-05 16:12:34 +0200

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿
+Import-Module posh-git
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
